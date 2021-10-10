69b7df30cd01db881fbf6d08739354e0b771db0c
NerdsAreIn <annacantthinkofagoodaddress@hotmail.co.uk>
2021-10-11 00:02:48 +0100

    more tinkering

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import './rest.css';
-import switchTabs from './tab-switching.js';
-import './images/Crocus.jpg';
-import {movingFunctions} from './moveImage.js';
+import {switchTabs} from './tab-switching.js';
+import crocusPhoto from './images/Crocus.jpg';
+import {moveImage, removeImage, crocuses} from './moveImage.js';
 
-//let result = window.matchMedia("(max-width: 650px)");
+let result = window.matchMedia("(max-width: 650px)");
 //const crocuses = document.getElementById("crocuses");
+crocuses.src = crocusPhoto;
 //const tabPages = document.querySelectorAll(".tab-page");
 //const dividers = document.querySelectorAll(".divider");
 
-//moveImage();
-//removeImage();
+
+//moveImage(result);
+//removeImage(result);
+result.addEventListener("change", () => {moveImage();});
+result.addEventListener("change", () => {removeImage();});
 
 
 
--- src/moveImage.js
+++ src/moveImage.js
@@ -7,31 +7,44 @@ const dividers = document.querySelectorAll(".divider");
 // to see if the specified conditions are present. An event handler function can be added via <addListener> property
 // to continually run the search and refresh the results.
 let result = window.matchMedia("(max-width: 650px)");
- 
-const movingFunctions = (function () {
-	function moveImage(result){
-	 	if (result.matches) { 
-        for (let i = 0; i < tabPages.length; i++) {
-			if (!(tabPages[i].classList.contains("hidden"))) {
-                tabPages[i].insertBefore(crocuses, dividers[i]); 				
-                crocuses.setAttribute("style", "margin-top: 20px !important");
-			}
-  		}
-	}
-result.addListener(moveImage);
-}
-function removeImage(result) {
-	if (!(result.matches)) {
-		tabPages.forEach(tabPage => {
-			if (!(tabPage.classList.contains("hidden"))) {
-                tabPage.removeChild(crocuses);
-                document.body.appendChild(crocuses);
-                crocuses.setAttribute("style", "margin-top: revert");                      
-			}
-  		});
-	}
-result.addListener(removeImage);
-}
-})();
 
-export {movingFunctions};
+function moveImage(){
+	 	if (result.matches) { 
+			for (let i = 0; i < tabPages.length; i++) {
+				if (!(tabPages[i].classList.contains("hidden"))) {
+					tabPages[i].insertBefore(crocuses, dividers[i]); 				
+					crocuses.setAttribute("style", "margin-top: 20px !important");
+				}
+			}
+		}
+}
+
+function removeImage() {
+		if (!(result.matches)) {
+			tabPages.forEach(tabPage => {
+				if (!(tabPage.classList.contains("hidden"))) {
+					tabPage.removeChild(crocuses);
+					document.body.appendChild(crocuses);
+					crocuses.setAttribute("style", "margin-top: revert");                      
+				}
+			});
+		}		
+	}	
+ 	
+/*const movingFunctions = (function () {
+	
+	function removeImage(result) {
+		if (!(result.matches)) {
+			tabPages.forEach(tabPage => {
+				if (!(tabPage.classList.contains("hidden"))) {
+					tabPage.removeChild(crocuses);
+					document.body.appendChild(crocuses);
+					crocuses.setAttribute("style", "margin-top: revert");                      
+				}
+			});
+		}
+		result.addListener(removeImage);
+	}
+})();*/
+
+export {moveImage, removeImage, crocuses};
