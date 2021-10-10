48109b546d32d09250caed2a3a3cad6e87a06082
NerdsAreIn <annacantthinkofagoodaddress@hotmail.co.uk>
2021-10-10 17:49:35 +0100

    got image-moving functions working. Now need to fix image path.

--- src/index.js
+++ src/index.js
@@ -5,21 +5,35 @@ import './images/Crocus.jpg';
 
 let result = window.matchMedia("(max-width: 650px)");
 const crocuses = document.getElementById("crocuses");
-const tabPages = querySelectorAll(".tab-page");
+const tabPages = document.querySelectorAll(".tab-page");
+const dividers = document.querySelectorAll(".divider");
+console.log(tabPages);
 
 function moveImage(result){
  	if (result.matches) { 
+        for (let i = 0; i < tabPages.length; i++) {
+			if (!(tabPages[i].classList.contains("hidden"))) {
+                tabPages[i].insertBefore(crocuses, dividers[i]); 				
+                crocuses.setAttribute("style", "margin-top: 20px !important");
+			}
+  		}
+	}
+}
+
+function removeImage(result) {
+	if (!(result.matches)) {
 		tabPages.forEach(tabPage => {
-			if (tabPage.className !== "tab-page hidden") {
-                tabPage.appendChild(crocuses);
- 				//crocuses.setAttribute("style", "float: right");
-                //crocuses.style.marginTop = "20px";                              
+			if (!(tabPage.classList.contains("hidden"))) {
+                tabPage.removeChild(crocuses);
+                document.body.appendChild(crocuses);
+                crocuses.setAttribute("style", "margin-top: revert");                      
 			}
   		});
 	}
 }
 
 result.addListener(moveImage);
+result.addListener(removeImage)
 
 
 
