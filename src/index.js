25404a78de3228c45974b754f1a6108691d0f60f
NerdsAreIn <annacantthinkofagoodaddress@hotmail.co.uk>
2021-10-13 16:51:42 +0100

    more contact page tinkering. Comments box added.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,25 @@ import crocusPhoto from './images/Crocus.jpg';
 import {moveImage, removeImage, crocuses} from './moveImage.js';
 
 let result = window.matchMedia("(max-width: 650px)");
+const submit = document.querySelector("input[type=submit]");
+
+submit.onclick = () => {
+	console.log({submit});
+	preventDefault();
+}
+const charactersExceeded = document.createElement("p");
+const textBox = document.querySelector("textarea");
+const contactPage = document.getElementById("contact");
+charactersExceeded.style.color = "red";
+if (textBox.value.length > 300) {
+	contactPage.insertBefore(charactersExceeded, submit);
+}
+
 //const crocuses = document.getElementById("crocuses");
 crocuses.src = crocusPhoto;
 //const tabPages = document.querySelectorAll(".tab-page");
 //const dividers = document.querySelectorAll(".divider");
+
 
 
 //moveImage(result);
