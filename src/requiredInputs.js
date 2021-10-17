66f8cde7b2ef5f6f887aaff22377ccffa4a3c78a
NerdsAreIn <annacantthinkofagoodaddress@hotmail.co.uk>
2021-10-18 00:34:28 +0100

    At wit's end. Pre-submission form validation check should work, but doesn't.

--- src/requiredInputs.js
+++ src/requiredInputs.js
@@ -1,20 +1,33 @@
 const requiredInputs = document.querySelectorAll("[required]");
-const validNumber = /\d {11}/;
+const validNumber = /^0\d{10}/;
+let inputMessages = [];
+let invalidMessage = 0;
 import {submitForm} from './submission.js';
+export {checkRequiredInputs};
 
-function checkNumber(requiredInput) {
+function checkNumber(requiredInput, inputMessages) {
+    
+	let invalidNumberMessage;
 	if (!(requiredInput.value.match(validNumber))) {
-						let invalidNumberMessage = document.createElement("div");
-            			invalidNumberMessage.setAttribute("style", "color: white; border: 2px solid red; margin-top: -10px; border-radius: 10px; background-color: rgba(255, 0, 0, 0.4)");
-						invalidNumberMessage.innerHTML = "<p><b>!</b> You must enter a valid telephone number of 11 digits.</p>";				
-						requiredInput.parentElement.appendChild(invalidNumberMessage);
+        invalidNumberMessage = document.createElement("div");
+		invalidNumberMessage.setAttribute("style", "color: white; border: 2px solid red; margin-top: -10px; border-radius: 10px; background-color: rgba(255, 0, 0, 0.4)");
+		invalidNumberMessage.innerHTML = "<p><b>!</b> You must enter a valid telephone number of 11 digits.</p>";				
+		requiredInput.parentElement.appendChild(invalidNumberMessage);
+		++invalidMessage;
+	}
+	else if (requiredInput.value.match(validNumber)) {
+        if (requiredInput.parentElement.contains(invalidNumberMessage)) {
+		requiredInput.parentElement.removeChild(invalidNumberMessage);
+		}
+		invalidMessage = -1;
+        console.log({invalidMessage});
+		return invalidMessage;	
+	}
 }
-
 
 // TODO: Want to find a more elegant solution to counting the messages and making the submission contingent on there being none, but this just about works for now:
 function checkRequiredInputs(e) {
-    let inputRequiredMessage;
-    let inputMessages = 0;
+    let inputRequiredMessage;    
     e.preventDefault();  
 	for (let i = 0; i < requiredInputs.length; i++) {  
 		if (requiredInputs[i].value == "" && requiredInputs[i].parentElement.children.length < 4) {
@@ -23,26 +36,26 @@ function checkRequiredInputs(e) {
             inputRequiredMessage.setAttribute("style", "color: white; border: 2px solid red; margin-top: -10px; border-radius: 10px; background-color: rgba(255, 0, 0, 0.4)")
 			inputRequiredMessage.innerHTML = "<p><b>!</b> You need to fill out this field.</p>";				
 			requiredInputs[i].parentElement.appendChild(inputRequiredMessage);
-			++inputMessages;      
+			inputMessages[i] = 1;      
 			console.log(inputMessages);				
 		}  
-		else if (requiredInputs[i].value != "")  {
-			--inputMessages;		
+		else if (requiredInputs[i].value != "")  {					
 			if (requiredInputs[i].parentElement.children.length > 3 ) {
-				requiredInputs[i].parentElement.lastElementChild.remove();						console.log(inputMessages);              		
+				requiredInputs[i].parentElement.lastElementChild.remove();
+				inputMessages[i] = 0;		
+				console.log(inputMessages);              		
 				if (requiredInputs[i].id == "tel") {
-					that = requiredInputs[i];  
-					checkNumber(that);
-				}
-				else return;
-			}  
+					const that = requiredInputs[i]; 
+                   	checkNumber(that);
+				}				
 		}
 	}
-	if (inputMessages <= -4) {
-        //console.log("No message!");
+	}
+    let valid = inputMessages.toString();
+console.log (inputMessages.toString());
+	if (valid == "0,0,0,0 ") {
+        //console.log("No message!");		
 		submitForm(e);
         return;
 	}
 }
-
-export {checkRequiredInputs};
