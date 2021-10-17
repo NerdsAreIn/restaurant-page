8e4ffc69c5c2086884cbfceab26f13ee6b1dbd15
NerdsAreIn <annacantthinkofagoodaddress@hotmail.co.uk>
2021-10-17 01:44:30 +0100

    restyled and refined validation messages

--- src/requiredInputs.js
+++ src/requiredInputs.js
@@ -1,5 +1,15 @@
 const requiredInputs = document.querySelectorAll("[required]");
+const validNumber = /\d {11}/;
 import {submitForm} from './submission.js';
+
+function checkNumber(requiredInput) {
+	if (!(requiredInput.value.match(validNumber))) {
+						let invalidNumberMessage = document.createElement("div");
+            			invalidNumberMessage.setAttribute("style", "color: white; border: 2px solid red; margin-top: -10px; border-radius: 10px; background-color: rgba(255, 0, 0, 0.4)");
+						invalidNumberMessage.innerHTML = "<p><b>!</b> You must enter a valid telephone number of 11 digits.</p>";				
+						requiredInput.parentElement.appendChild(invalidNumberMessage);
+}
+
 
 // TODO: Want to find a more elegant solution to counting the messages and making the submission contingent on there being none, but this just about works for now:
 function checkRequiredInputs(e) {
@@ -9,19 +19,22 @@ function checkRequiredInputs(e) {
 	for (let i = 0; i < requiredInputs.length; i++) {  
 		if (requiredInputs[i].value == "" && requiredInputs[i].parentElement.children.length < 4) {
 			//console.log("no message");
-			inputRequiredMessage = document.createElement("div");
-			inputRequiredMessage.style.color = "red";
-			inputRequiredMessage.innerHTML = "<p>You need to fill out this field.</p>";				
+			inputRequiredMessage = document.createElement("div");			
+            inputRequiredMessage.setAttribute("style", "color: white; border: 2px solid red; margin-top: -10px; border-radius: 10px; background-color: rgba(255, 0, 0, 0.4)")
+			inputRequiredMessage.innerHTML = "<p><b>!</b> You need to fill out this field.</p>";				
 			requiredInputs[i].parentElement.appendChild(inputRequiredMessage);
-			inputRequiredMessage;
-			inputMessages += 1;      
-			//console.log(inputMessages);				
+			++inputMessages;      
+			console.log(inputMessages);				
 		}  
 		else if (requiredInputs[i].value != "")  {
-			//console.log("I've got a message!"); 
-			inputMessages -= 1;		
+			--inputMessages;		
 			if (requiredInputs[i].parentElement.children.length > 3 ) {
-				requiredInputs[i].parentElement.lastElementChild.remove();						//console.log(inputMessages);              		
+				requiredInputs[i].parentElement.lastElementChild.remove();						console.log(inputMessages);              		
+				if (requiredInputs[i].id == "tel") {
+					that = requiredInputs[i];  
+					checkNumber(that);
+				}
+				else return;
 			}  
 		}
 	}
